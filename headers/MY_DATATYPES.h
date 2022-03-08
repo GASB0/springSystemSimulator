c0300d4d562885821b2a2e02ceab7880012c7553
Gabriel Santos <santos.baez.gabriel.elo@gmail.com>
2022-03-07 20:25:00 -0400

    Primeros cálculos de las propiedades de parículas
    La asignación de los vecinos a cada partícula es un mayor cuello de
    botella, por lo que hay que refactorizar esa sección del programa.
    
    En la asignación del término de los springs que conecta una partícula
    con sus vecinos haces la asunción de que todos estos tienen exactamente
    la misma longitud. Los springs que van a dar a las esquinas del cuadro
    de vecionos tienen una longitud sqrt(2)/2 veces más grande que el que
    les queda directamente de un lado, o dirctamente arriba o abajo.
    
    Estoy casi seguro que el acceso a las partículas por medio de su
    llamada en el arreglo de parículas (particleSystem) cada vez que se
    quiere operar sobre la partícula ij consume más tiempo de lo que
    sería prudente. Para el próximo commit debería tratar de buscar
    una solución para esto.

---
+++ headers/MY_DATATYPES.h
@@ -0,0 +1,185 @@
+#ifndef MY_DATATYPES
+#include <array>
+#include <assert.h>
+#include <chrono>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <tuple>
+#include <vector>
+template <class DT = std::chrono::milliseconds,
+          class ClockT = std::chrono::steady_clock>
+class Timer {
+  using timep_t = decltype(ClockT::now());
+
+  timep_t _start = ClockT::now();
+  timep_t _end = {};
+
+public:
+  void tick() {
+    _end = timep_t{};
+    _start = ClockT::now();
+  }
+
+  void tock() { _end = ClockT::now(); }
+
+  template <class duration_t = DT> auto duration() const {
+    // Use gsl_Expects if your project supports it.
+    assert(_end != timep_t{} && "Timer must toc before reading the time");
+    return std::chrono::duration_cast<duration_t>(_end - _start);
+  }
+};
+
+struct vec3 {
+public:
+  float x, y, z;
+  template <typename T> T operator=(const T aTriple) {
+    x = aTriple.x;
+    y = aTriple.y;
+    z = aTriple.z;
+    return {x = aTriple.x, y = aTriple.y, z = aTriple.z};
+  };
+
+  template <typename T> T operator+(const T &aTriple) {
+    return {aTriple.x + x, aTriple.y + y, aTriple.z + z};
+  };
+
+  template <typename T> T operator+=(const T &aTriple) {
+    return {x += aTriple.x, y += aTriple.y, z += aTriple.z};
+  };
+
+  template <typename T> T operator-(const T &aTriple) {
+    return {x - aTriple.x, y - aTriple.y, z - aTriple.z};
+  };
+
+  template <typename T> T operator-=(const T &aTriple) {
+    return {x -= aTriple.x, y -= aTriple.y, z -= aTriple.z};
+  };
+
+  vec3 operator+(const float &aScalar) {
+    return {aScalar + x, aScalar + y, aScalar + z};
+  };
+
+  vec3 operator*(const float &aScalar) {
+    return {aScalar * x, aScalar * y, aScalar * z};
+  };
+
+  vec3 operator*=(const float &aScalar) {
+    return {x *= aScalar, y *= aScalar, z *= aScalar};
+  };
+
+  vec3 operator/(const float &aScalar) {
+    return {x / aScalar, y / aScalar, z / aScalar};
+  };
+
+  vec3 operator/=(const float &aScalar) {
+    return {x /= aScalar, y /= aScalar, z /= aScalar};
+  };
+
+  float norm() {
+    float norm = std::sqrt(std::pow(x, 2) + std::pow(y, 2) + std::pow(z, 2));
+    return norm;
+  }
+
+  vec3 direction() { return {x / norm(), y / norm(), z / norm()}; }
+};
+
+// Recuerda de tus clases de física que con sólo la posición y
+// la velocidad puedes determinar el estado de una partícula.
+class particle {
+public:
+  //------------------------------------------------------//
+  // Propiedades físicas de nuestra partícula
+  //------------------------------------------------------//
+  struct position : vec3 {
+    template <typename T> T operator=(const T &aTriplet) {
+      x = aTriplet.x;
+      y = aTriplet.y;
+      z = aTriplet.z;
+      return {x + aTriplet.x, y + aTriplet.y, z + aTriplet.z};
+    };
+  } pos;
+
+  struct velocity : public vec3 {
+    template <typename T> T operator=(const T &aTriplet) {
+      x = aTriplet.x;
+      y = aTriplet.y;
+      z = aTriplet.z;
+      return {x + aTriplet.x, y + aTriplet.y, z + aTriplet.z};
+    };
+  } vel;
+
+  struct netForce : vec3 {
+    template <typename T> T operator=(const T &aTriplet) {
+      x = aTriplet.x;
+      y = aTriplet.y;
+      z = aTriplet.z;
+      return {x + aTriplet.x, y + aTriplet.y, z + aTriplet.z};
+    };
+  } netFrc;
+
+  struct matrixPos {
+    int i, j;
+  } matPos;
+
+  float mass;
+  // Partículas aledañas a nuestra partícula
+  std::vector<particle> neighbours;
+
+  // Esta función recibe como argumento un vector de
+  // tuplas con las "posiciones relativas" correspondientes
+  // a los vecinos que se espera que tenga la partícula
+  void setRelativeNeighbours(
+      // vez que los llamas.
+      std::vector<std::tuple<int, int>> &RelativeNeighboursList,
+      std::vector<std::vector<particle>> &aParticleSystem) {
+
+    int p, q;
+
+    for (int i = 0; i < (int)RelativeNeighboursList.size(); i++) {
+      p = std::get<0>(RelativeNeighboursList.at(i));
+      q = std::get<1>(RelativeNeighboursList.at(i));
+      try {
+        // TODO: Cambiar esto por punteros/referencias.
+        // neighbours.push_back(aParticleSystem.at(matPos.i + p).at(matPos.j +
+        // q));
+        neighbours.push_back(aParticleSystem.at(matPos.i + p).at(matPos.j + q));
+      } catch (const std::out_of_range &e) {
+      }
+    }
+
+    neighbours.shrink_to_fit();
+  }
+
+  particle(std::tuple<float, float, float> position,
+           std::tuple<float, float, float> velocity,
+           std::tuple<float, float, float> force, float aMass,
+           std::tuple<int, int> matPosition) {
+    // Inicializando las posiciones
+    pos.x = std::get<0>(position);
+    pos.y = std::get<1>(position);
+    pos.z = std::get<2>(position);
+
+    // Inicializando las velocidades
+    vel.x = std::get<0>(velocity);
+    vel.y = std::get<1>(velocity);
+    vel.z = std::get<2>(velocity);
+
+    // Inicializando las fuerzas
+    netFrc.x = std::get<0>(force);
+    netFrc.y = std::get<1>(force);
+    netFrc.z = std::get<2>(force);
+
+    // Inicializando la masa
+    mass = aMass;
+
+    // Inicializando las posiciones en la "matriz"
+    matPos.i = std::get<0>(matPosition);
+    matPos.j = std::get<1>(matPosition);
+
+    // Reservando la memoria que creo que usaré
+    neighbours.reserve(8);
+  }
+};
+
+#endif
--- src.cpp
+++ src.cpp
@@ -1,85 +1,157 @@
+#include "./headers/MY_DATATYPES.h"
 #include <array>
-#include <fstream>
+#include <assert.h>
+#include <chrono>
+#include <cmath>
+#include <cstdio>
 #include <iostream>
-#include <istream>
-#include <ostream>
 #include <tuple>
 #include <vector>
-
-// Recuerda de tus clases de física que con sólo la posición y
-// la velocidad puedes determinar el estado de una partícula.
-
-class particle {
-public:
-  // Propiedades físicas de nuestra partícula
-  struct position {
-    float x, y, z;
-  } pos;
-
-  struct velocity {
-    float x, y, z;
-  } vel;
-
-  // Partículas aledañas a nuestra partícula
-  struct neighbours {
-    particle *upNeighbour;
-    particle *downNeighbour;
-    particle *leftNeighbour;
-    particle *rightNeighbour;
-    //
-    particle *upleft;
-    particle *upright;
-    particle *downleft;
-    particle *downright;
-  } neigh;
-
-  particle(std::tuple<float, float, float> position,
-           std::tuple<float, float, float> velocity) {
-    // Inicializando las posiciones
-    pos.x = std::get<0>(position);
-    pos.y = std::get<1>(position);
-    pos.z = std::get<2>(position);
-
-    // Inicializando las velocidades
-    vel.x = std::get<0>(velocity);
-    vel.x = std::get<1>(velocity);
-    vel.x = std::get<2>(velocity);
-  }
-};
 
 // Dimensiones de nuestra cuadrilla
 int width, height;
 
 int main() {
+  width = 3;
+  height = 3;
+
   std::vector<particle> elements;
   std::vector<std::vector<particle>> particleSystem;
+  elements.reserve(width);
+  particleSystem.reserve(height);
 
-  // Inicialización de las partículas en una cuadrilla
-  // rectangular:
-  width = 10;
-  height = 10;
-
-  auto apos = std::make_tuple(0.0, 0.0, 0.0);
-  auto avel = std::make_tuple(0.0, 0.0, 0.0);
+  std::tuple<int, int> aMatPos;
+  std::tuple<float, float, float> apos, avel, aforce;
+  avel = std::make_tuple((float)0.0, (float)0.0, (float)0.0);
+  aforce = std::make_tuple((float)0.0, (float)0.0, (float)0.0);
+  float mass = 1;
 
   for (int i = 0; i < height; i++) {
     elements.clear();
     for (int j = 0; j < width; j++) {
-      apos = std::make_tuple((float)i, (float)j, 0.0);
-      elements.push_back(particle(apos, avel));
+      apos = std::make_tuple((float)j, (float)(height - (i + 1)),
+                             0.0); // Esta cosa son las coordenadas en
+                                   // el plano cartesiano R^3 x,y,z.
+
+      aMatPos = std::make_tuple(i, j); // Esta cosa son la "posición" de las
+                                       // partículas en una matrix para su
+                                       // acceso.
+
+      elements.push_back(
+          particle(apos, avel, aforce, mass, aMatPos)); // Inicialización de
+                                                        // la partícula P_ij.
     }
     particleSystem.push_back(elements);
   }
 
-  // // Test del almacenamiento de estos valores
-  // // Funciona, xdxd
-  // for (int i = 0; i < height; i++) {
-  //   for (int j = 0; j < width; j++) {
-  //     std::cout << particleSystem[i][j].pos.x << ","
-  //               << particleSystem[i][j].pos.y << "\t";
-  //   }
-  //   std::cout << std::endl;
-  // }
+  //------------------------------------------------------//
+  // Asignación de vecinos con respecto a la posición en matriz
+  //------------------------------------------------------//
+  std::tuple<int, int> foo1(-1, 0);  // Up
+  std::tuple<int, int> foo2(1, 0);   // Down
+  std::tuple<int, int> foo3(0, -1);  // Left
+  std::tuple<int, int> foo4(0, 1);   // Right
+  std::tuple<int, int> foo5(-1, -1); // UpLeft
+  std::tuple<int, int> foo6(-1, 1);  // UpRight
+  std::tuple<int, int> foo7(1, -1);  // DownLeft
+  std::tuple<int, int> foo8(1, 1);   // DownRight
+
+  std::vector<std::tuple<int, int>> listaVecinos;
+  listaVecinos.reserve(8);
+  listaVecinos.push_back(foo1);
+  listaVecinos.push_back(foo2);
+  listaVecinos.push_back(foo3);
+  listaVecinos.push_back(foo4);
+  listaVecinos.push_back(foo5);
+  listaVecinos.push_back(foo6);
+  listaVecinos.push_back(foo7);
+  listaVecinos.push_back(foo8);
+
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      particleSystem.at(i).at(j).setRelativeNeighbours(listaVecinos,
+                                                       particleSystem);
+    }
+  }
+
+  //------------------------------------------------------//
+  // Calculando las fuerzas sobre cada una de las partículas
+  //------------------------------------------------------//
+  vec3 ParticleNetForce = {0.0, 0.0, 0.0};
+  vec3 netNeighbourForce = {0.0, 0.0, 0.0};
+  vec3 gForce = {0.0, -9.8, 0.0};
+  vec3 extForces = {0.0, 0.0, 0.0};
+
+  const float springsLength = 1.0; // Longitud de los "resortes"
+  const float K = 1;               // Constante de los resortes
+  float dt = 0.01;
+
+  (void)springsLength;
+  Timer<std::chrono::microseconds, std::chrono::steady_clock> timer;
+
+  for (int m = 0; m < 100000; m++) {
+    // TODO: Calcular el diferencial de tiempo para cada iteración del loop en
+    // donde se estará ejecutando la simulación
+    timer.tick();
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+
+        // Calculando la fuerza neta ejercida por los vecinos a mi partícula
+        netNeighbourForce = {0.0, 0.0, 0.0};
+        for (int neighInd = 0;
+             neighInd < (int)particleSystem.at(i).at(j).neighbours.size();
+             neighInd++) {
+
+          // Encontrando la dirección de la fuerza:
+          // TODO: Revisar si esto realmente funciona
+          vec3 springTerm =
+              ((vec3){0.0, 0.0, 0.0} -
+               (particleSystem.at(i).at(j).neighbours.at(neighInd).pos -
+                particleSystem.at(i).at(j).pos)
+                   .direction()) *
+              springsLength;
+
+          netNeighbourForce +=
+              (particleSystem.at(i).at(j).neighbours.at(neighInd).pos -
+               (particleSystem.at(i).at(j).pos) + springTerm) *
+              K;
+        }
+
+        // Añadiendo la fuerza de la gravedad
+        gForce = {0.0, -9.8, 0.0};
+
+        // Añadiendo fuerza externa
+        extForces = {0.0, 0.0, 0.0}; // No vamos a tener
+                                     // ninguna fuerza
+                                     // externa por estos momentos
+
+        // Fuerza neta en la partícula(ij)
+        ParticleNetForce =
+            gForce + netNeighbourForce + extForces; // Esta es la fuerza
+                                                    // que tenemos en esta
+                                                    // partícula en este
+                                                    // instante.
+
+        // Calculando la velcidad de la partícula
+        particleSystem.at(i).at(j).vel =
+            ((particleSystem.at(i).at(j).netFrc + ParticleNetForce) /
+             (float)2) *
+            dt / particleSystem.at(i).at(j).mass;
+
+        // Actualizando la fuerza que sufre la patícula luego de haber calculado
+        // su nueva velocidad.
+        particleSystem.at(i).at(j).netFrc = ParticleNetForce;
+
+        // Calculando la posición de la partícula
+        particleSystem.at(i).at(j).pos += particleSystem.at(i).at(j).vel * dt;
+      }
+      std::cout << "\n";
+      std::cout << "\n";
+    }
+    timer.tock();
+    dt = timer.duration().count() / (float)1000000; // Recalculando el
+                                                    // diferencial de tiempo
+  }
 
   return 0;
 }
